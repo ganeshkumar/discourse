682bf56262dae10fbb59508246b305cfad2470e6
Ganesh Kumar <ganesh.kumar@people10.com>
2014-02-03 18:07:17 +0530

    added smoke_free_urls to discourse

--- config/initializers/omniauth.rb
+++
@@ -1,7 +0,0 @@
-APP_ID =  'a57gewjdndfuerjfdfdn8u'
-
-APP_SECRET  = "skhd73y7yrfjh98rejenfdiyhy7eu33j43jwd72j3e"
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :smoke_free_project_strategy, APP_ID, APP_SECRET
-end
