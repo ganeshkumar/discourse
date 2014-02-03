cf51f46bd30b53b0ded2de52f6bc445672ba2c3f
Ganesh Kumar <ganesh.kumar@people10.com>
2014-02-03 22:57:06 +0530

    added Rails.root

--- config/initializers/smoke_free_project_config.rb
+++ config/initializers/smoke_free_project_config.rb
@@ -3,4 +3,4 @@ APP_ID =  'a57gewjdndfuerjfdfdn8u'
 APP_SECRET  = "skhd73y7yrfjh98rejenfdiyhy7eu33j43jwd72j3e"
 
 
-SMOKE_FREE_URLS = YAML.load_file("#{RAILS_ROOT}/config/smoke_free_url_config.yml")[Rails.env]
+SMOKE_FREE_URLS = YAML.load_file("#{Rails.root}/config/smoke_free_url_config.yml")[Rails.env]
