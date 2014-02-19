f27ccd4e4b0c74d8f7cfdfa4d3b3c7741d2b0384
Ganesh Kumar <ganesh.kumar@people10.com>
2014-02-20 00:41:10 +0530

    add : appid and secret

--- config/initializers/smoke_free_project_config.rb
+++ config/initializers/smoke_free_project_config.rb
@@ -1,6 +1,10 @@
-DEVELOPMENT_SMOKE_FREE_URL = "http://localhost:3000"
+DEVELOPMENT_SMOKE_FREE_URL = "http://localhost:9090"
 
 PROFILE_SMOKE_FREE_URL = "http://95.85.57.155"
 
 
 PRODUCTION_SMOKE_FREE_URL = "http://smokefreeproject.org"
+
+APP_ID =  'a57gewjdndfuerjfdfdn8u'
+
+APP_SECRET  = "skhd73y7yrfjh98rejenfdiyhy7eu33j43jwd72j3e"
