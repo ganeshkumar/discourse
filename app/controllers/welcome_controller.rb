2b67c5b6ab519ba0f12ce0be229a8070b5fb33ab
Ganesh Kumar <ganesh.kumar@people10.com>
2014-02-20 00:10:51 +0530

    removed commented code

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -11,15 +11,15 @@ class WelcomeController < ApplicationController
   skip_before_filter :redirect_to_login_if_required
 
   def authorize_user_from_smoke_free
-    #url = "?client_id=#{APP_ID}&client_secret=#{APP_SECRET}&state=active&auth_token=#{params[:auth_token]}"
-    #response = HTTParty.get("http://95.85.57.155/auth/smoke_free_strategy/access_token.json#{url}")
-    #parsed_response = JSON.parse(response.body)
-    #if parsed_response["user"].present?
-      login_user(params)
+    url = "?client_id=#{APP_ID}&client_secret=#{APP_SECRET}&state=active&auth_token=#{params[:auth_token]}"
+    response = HTTParty.get("#{SMOKE_FREE_URLS["url"]}/auth/smoke_free_strategy/access_token.json#{url}")
+    parsed_response = JSON.parse(response.body)
+    if parsed_response["user"].present?
+      login_user(parsed_response["user"])
       redirect_to "/"
-    #else
-    #  redirect_to "#{SMOKE_FREE_URLS["url"]}"
-    #end
+    else
+      redirect_to "#{SMOKE_FREE_URLS["url"]}"
+    end
   end
 
 
@@ -27,17 +27,16 @@ class WelcomeController < ApplicationController
   private
 
   def login_user(user_param)
-    user = User.first
+    user = User.find_by_email(user_param["email"])
     if user.blank?
-     user =  User.new({:name => user_param["nickname"], :username => user_param["nickname"],
-                       :email => user_param["email"]})
-     user.save!
-     user.reload
-     user.update_column(:active, true)
+      user =  User.new({:name => user_param["nickname"], :username => user_param["nickname"],
+                        :email => user_param["email"]})
+      user.save!
+      user.reload
+      user.update_column(:active, true)
     end
     log_on_user(user)
   end
-
 
   def user_params
   params.permit(
