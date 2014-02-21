8feeff2d24f9767f6edbd719f3684cfe7288e0c1
Ganesh Kumar <ganesh.kumar@people10.com>
2014-02-21 23:38:59 +0530

    added the forum.smokefreeproject.org to produciton

--- config/initializers/smoke_free_project_config.rb
+++ config/initializers/smoke_free_project_config.rb
@@ -1,3 +1,6 @@
+###############################################################
+##################Urls for Smokefree project###################
+###############################################################
 DEVELOPMENT_SMOKE_FREE_URL = "http://localhost:9090"
 
 PROFILE_SMOKE_FREE_URL = "http://95.85.57.155"
@@ -8,3 +11,11 @@ PRODUCTION_SMOKE_FREE_URL = "http://smokefreeproject.org"
 APP_ID =  'a57gewjdndfuerjfdfdn8u'
 
 APP_SECRET  = "skhd73y7yrfjh98rejenfdiyhy7eu33j43jwd72j3e"
+
+
+###############################################################
+##################Urls for Baseurl[Discourse]##################
+###############################################################
+PROFILE_ENV_BASE_URL = "95.85.63.95"
+PRODUCTION_ENV_BASE_URL = "forum.smokefreeproject.org"
+DEVELOPMENT_ENV_BASE_URL = "localhost"
